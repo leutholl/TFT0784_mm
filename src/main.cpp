c31958546b9bad16b74debce693ff557f1d2a2bd
leutholl <leutholl@gmail.com>
2024-04-23 17:52:42 +0200

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,30 +17,29 @@
  *     -- The GT911 is typically already flashed out of factory with the correct resolution and config. You can reflash it with the GT911 library but first patch the Wire library for super large I2C buffers.
  * - with a little animation to show how to use lvgl buffes with the RA8876 BTE&ROP feature.
  * - uses RA8876 internal PWM for backlight control. Solder the jumpers on the ER-TFT0784 to enable internal PWM backlight control.
- * 
+ *
  * - This works on a MicroMod Teensy with the ATP board. The ER-TFT0784 is connected to the ATP board with Dupont cables. Using 20 MHz is working well.
- * 
+ *
  * with all the optimizations, the display is very responsive and fast. The lvgl animations are smooth and the touch input is very responsive.
  * The demo (a red ball sliding back and forth, which changes in size) and setting 40 FPS in lv_conf.h is running at 40 FPS with about 5 % CPU load.
  * Because the ball is changing size, lvgl buffer needs to sent in multiple junks when the ball is in the middle of the screen. Areas where the ball left must be updated as well.
  * The ball grows up to 200 pixels which is half of the screen height. This is a lot of pixels and a lot of things must be done right for a clean drawing.
  * Compared to SPI with DMA the same demo will run at around only 4 fps. Some lvgl ui components use animation and this is where this project shines.
- * 
+ *
  * The whole thread of ups and downs is here: https://github.com/wwatson4506/Ra8876LiteTeensy/issues/16
- * 
+ *
  * You can read more about the work in the library headers files.
- * 
+ *
  */
 //**************************************************************//
-
 
 #include <Arduino.h>
 #include <Metro.h>
 #include <lvgl.h>
 
 #include "ER-TFT0784_8080.hpp"
-#include "Wire.h"
 #include "GT911.h"
+#include "Wire.h"
 
 // Touch
 #define GT911_INT_PIN 29
@@ -61,17 +60,17 @@ GT911 touch = GT911();
     Pin 9 => FlexIO2:11 D7
 
     You must not change these pins as they are hardcoded in the library and FlexIO2 needs it that way.
-    
+
     You can change the other additional pins as you like here:
 */
 
-#define _2828_CS_PIN   2
-#define _2828_RST_PIN  3
-#define _2828_SDI_PIN  4
-#define _2828_SCK_PIN  5
-#define _8876_CS_PIN   16
-#define _8876_DC_PIN   11
-#define _8876_RST_PIN  17
+#define _2828_CS_PIN 2
+#define _2828_RST_PIN 3
+#define _2828_SDI_PIN 4
+#define _2828_SCK_PIN 5
+#define _8876_CS_PIN 16
+#define _8876_DC_PIN 11
+#define _8876_RST_PIN 17
 #define _8876_WAIT_PIN 26  // The library uses this pin to wait for the RA8876 to be ready instead of polling the Status Register. Connect to WINT pin.
 
 #define SCREENWIDTH 1280
@@ -81,9 +80,9 @@ ER_TFT0784_8080 TFT = ER_TFT0784_8080(_2828_CS_PIN, _2828_RST_PIN, _2828_SDI_PIN
 
 #define BUFFER_SIZE (SCREENWIDTH * SCREENHEIGHT / 10)  // About 10th of the screen is large enough. If very small, the BTE&ROP overhead is too large.
 
-#define LVGL_DRAW_8876_CANVAS 1 // Which RA8876 page or canvas lvgl shall draw to. Some projects use multiple canvases and combine them later.
-#define VISIBLE_8876_CANVAS 1   // Which RA8876 page or canvas is visible. This is the canvas that is shown on the screen. It can be different from the drawing canvas.
-#define NON_LVGL_DRAW_8876_CANVAS 2 // If you have multiple canvases, you can draw to one and then combine them to the visible canvas.
+#define LVGL_DRAW_8876_CANVAS 1      // Which RA8876 page or canvas lvgl shall draw to. Some projects use multiple canvases and combine them later.
+#define VISIBLE_8876_CANVAS 1        // Which RA8876 page or canvas is visible. This is the canvas that is shown on the screen. It can be different from the drawing canvas.
+#define NON_LVGL_DRAW_8876_CANVAS 2  // If you have multiple canvases, you can draw to one and then combine them to the visible canvas.
 
 // lvgl stuff
 static lv_disp_draw_buf_t disp_buf;
@@ -92,7 +91,6 @@ static DMAMEM lv_color_t buf_2[BUFFER_SIZE] __aligned(32);
 static lv_disp_drv_t disp_drv;
 static lv_disp_t* disp;
 static lv_indev_drv_t indev_drv;
-
 
 DMAMEM uint16_t rotated_image[BUFFER_SIZE] __aligned(32);
 
@@ -120,7 +118,7 @@ FASTRUN void my_disp_flush(lv_disp_drv_t* disp, const lv_area_t* area, lv_color_
     }
 
     // note that XY are swapped for the RA8876 and width and height are swapped in the arguments
-    uint16_t newY = area->x1;                            // swap x to y
+    uint16_t newY = area->x1;                          // swap x to y
     uint16_t newX = SCREEN_WIDTH - area->y1 - height;  // swap y to x and reposition
 
     TFT.bteMpuWriteWithROPData16_MultiBeat_DMA(LVGL_DRAW_8876_CANVAS, SCREEN_WIDTH, newX, newY,                 // Source 1 is ignored for ROP 12
@@ -128,14 +126,14 @@ FASTRUN void my_disp_flush(lv_disp_drv_t* disp, const lv_area_t* area, lv_color_
                                                RA8876_BTE_ROP_CODE_12,
                                                rotated_image);
 
-    //while (TFT.WR_DMATransferDone == false) {} //Wait for any DMA transfers to complete. This makes everything synchronous and slow. It nullifies the advantage of DMA. CPU load is 5 times higher.
+    // while (TFT.WR_DMATransferDone == false) {} //Wait for any DMA transfers to complete. This makes everything synchronous and slow. It nullifies the advantage of DMA. CPU load is 5 times higher.
 
     isLastBuffer = lv_disp_flush_is_last(&disp_drv);
 }
 
-FASTRUN void DMAfinished() { // this is a callback of the LCD driver when DMA is finished. It is called from the ISR. LVGL needs to know when the DMA is done.
+FASTRUN void DMAfinished() {  // this is a callback of the LCD driver when DMA is finished. It is called from the ISR. LVGL needs to know when the DMA is done.
     lv_disp_flush_ready(&disp_drv);
-    //if (isLastBuffer) TFT.updateScreenWithROP(LVGL_DRAW_8876_CANVAS, NON_LVGL_DRAW_8876_CANVAS, VISIBLE_8876_CANVAS, RA8876_BTE_ROP_CODE_14); // if application uses multiple canvases (e.g. non LVGL Drawings), we combine them here after lvgl is done.
+    // if (isLastBuffer) TFT.updateScreenWithROP(LVGL_DRAW_8876_CANVAS, NON_LVGL_DRAW_8876_CANVAS, VISIBLE_8876_CANVAS, RA8876_BTE_ROP_CODE_14); // if application uses multiple canvases (e.g. non LVGL Drawings), we combine them here after lvgl is done.
 }
 
 void setupTouch() {
@@ -151,7 +149,8 @@ void setupTouch() {
     Wire.beginTransmission(touch.i2cAddr);
     int error = Wire.endTransmission();
     if (error != 0) {
-        Serial.print(": ERROR #"); Serial.println(error);
+        Serial.print(": ERROR #");
+        Serial.println(error);
     } else {
         Serial.println(": SUCCESS");
         // do config
@@ -235,7 +234,7 @@ void setup() {
 
     // TFT.Color_Bar_ON(); while (true); // this is a test pattern to check if the display is working. It should show a color bar. If not, check your wiring.
 
-    TFT.setRotation(3); // 270 degrees rotation for coordinates and scanning and memory write direction on the RA8876. Note that we will must rotate the lvgl buffer.
+    TFT.setRotation(3);  // 270 degrees rotation for coordinates and scanning and memory write direction on the RA8876. Note that we will must rotate the lvgl buffer.
 
     for (uint8_t i = 1; i < 10; i++) {
         TFT.useCanvas(i);
@@ -244,7 +243,7 @@ void setup() {
 
     // screen is set up. Now proceeding with lvgl setup.
     lv_init();
-    lv_disp_draw_buf_init(&disp_buf, buf_1, buf_2, BUFFER_SIZE); 
+    lv_disp_draw_buf_init(&disp_buf, buf_1, buf_2, BUFFER_SIZE);
 
     lv_disp_drv_init(&disp_drv);
     disp_drv.hor_res = SCREENWIDTH;
@@ -259,10 +258,10 @@ void setup() {
     disp = lv_disp_drv_register(&disp_drv);
 
     // initialize the input device driver
-    lv_indev_drv_init(&indev_drv);         
+    lv_indev_drv_init(&indev_drv);
     indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = my_touchpad_read;   
-    lv_indev_drv_register(&indev_drv);     
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
 
     // a little demo using animations
     lv_obj_t* obj = lv_obj_create(lv_scr_act());
@@ -289,7 +288,6 @@ void setup() {
     lv_anim_start(&a);
 
     Serial.println("Run Mode.");
-
 }
 
 void loop() {
